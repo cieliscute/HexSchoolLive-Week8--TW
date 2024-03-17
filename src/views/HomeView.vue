131dbfd47cba628cfbafcc05a9169856e4ae63af
kaiwei <cielchang0324@gmail.com>
2024-03-18 03:03:16 +0800

    feat: 🎸 完成tailwind & shadcn初步設定。並測試tailwind css & shadcn 元件正確運行

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,29 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="text-orange-700">我是h1</h1>
+  <Carousel class="relative w-full max-w-xs ms-20" :opts="{
+    align: 'start',
+  }">
+    <CarouselContent>
+      <CarouselItem v-for="(_, index) in 5" :key="index" class="md:basis-1/2 lg:basis-1/3">
+        <div class="p-1">
+          <Card>
+            <CardContent class="flex aspect-square items-center justify-center p-6">
+              <span class="text-3xl font-semibold">{{ index + 1 }}</span>
+            </CardContent>
+          </Card>
+        </div>
+      </CarouselItem>
+    </CarouselContent>
+    <CarouselPrevious />
+    <CarouselNext />
+  </Carousel>
 </template>
+<script setup>
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel'
+</script>
